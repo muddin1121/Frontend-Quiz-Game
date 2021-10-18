5a567c4f0072edf9a648e6833d3d49d04e3a17a1
muddin1121 <muddin1121@gmail.com>
2021-10-17 23:28:04 -0400

    just added the chached elemnt references to all the buttons, and added a reset button. Created event listeners to test the working buttons

--- js/app.js
+++ js/app.js
@@ -10,6 +10,37 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
+const htmlBtn = document.querySelector("#html-button")
+const cssBtn = document.querySelector("#css-button")
+const javascriptBtn = document.querySelector("#javascript-button")
+const createBtn = document.querySelector("#create-button")
+const resetBtn = document.querySelector("#reset-button")
+
+const quizContainer = document.querySelector("#quiz-container")
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+htmlBtn.addEventListener('click', () => {
+  console.log('HTML button works!')
+})
+
+cssBtn.addEventListener('click', () => {
+  console.log('CSS button works!')
+})
+
+javascriptBtn.addEventListener('click', () => {
+  console.log('JavaScript button works!')
+})
+
+createBtn.addEventListener('click', () => {
+  console.log('Create button works!')
+})
+
+resetBtn.addEventListener('click', () => {
+  console.log('Reset button works!')
+})
+
 
 
 /*-------------------------------- Functions --------------------------------*/
