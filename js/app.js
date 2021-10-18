5d8c6117f9990a1763410262cebfcbc9bafc6a5f
muddin1121 <muddin1121@gmail.com>
2021-10-18 00:02:20 -0400

    added 4 event listeners and cached elements for the four answer buttons

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+//question buttons
 const htmlBtn = document.querySelector("#html-button")
 const cssBtn = document.querySelector("#css-button")
 const javascriptBtn = document.querySelector("#javascript-button")
@@ -18,8 +18,16 @@ const resetBtn = document.querySelector("#reset-button")
 
 const quizContainer = document.querySelector("#quiz-container")
 
+//ANSWER BUTTONS
+const ans1 = document.querySelector("#answer-1")
+const ans2 = document.querySelector("#answer-2")
+const ans3 = document.querySelector("#answer-3")
+const ans4 = document.querySelector("#answer-4")
+
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+// REGULAR BUTTONS
 
 htmlBtn.addEventListener('click', () => {
   console.log('HTML button works!')
@@ -39,6 +47,25 @@ createBtn.addEventListener('click', () => {
 
 resetBtn.addEventListener('click', () => {
   console.log('Reset button works!')
+})
+
+//ANSWER BUTTONS
+
+
+ans1.addEventListener('click', () => {
+  console.log('answer 1 button works!')
+})
+
+ans2.addEventListener('click', () => {
+  console.log('answer 2 button works!')
+})
+
+ans3.addEventListener('click', () => {
+  console.log('answer 3 button works!')
+})
+
+ans4.addEventListener('click', () => {
+  console.log('answer 4 button works!')
 })
 
 
